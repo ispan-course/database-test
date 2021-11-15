91b4338934be49da87bfc770d0c5fd4716d7f1ec
Arthur Pai <baidragoon@gmail.com>
2021-11-15 17:19:09 +0800

    feat: execute non-query command

--- DatabaseTest/Program.cs
+++ DatabaseTest/Program.cs
@@ -80,6 +80,7 @@ namespace DatabaseTest
               var obj = reader.GetValue(i);
               Console.Write("{0}: {1}, ", name, obj.ToString());
             }
+
             Console.WriteLine("");
           }
         } while (reader.NextResult());
@@ -89,6 +90,44 @@ namespace DatabaseTest
       else
       {
         Console.Write("Query Failed!");
+        return false;
+      }
+
+      // first insert
+      var id1 = (int)( System.DateTime.Now.Ticks & 0x00FFFFFF );
+      var date1 = System.DateTime.Now.ToString( "yyyy/MM/dd" );
+      var rows = ExecuteNonQuery($"INSERT INTO member(`id`, `name`, `dob`) VALUES({id1}, {id1}, \"{date1}\")");
+      if( rows != 1 )
+      {
+        Console.Write( "Insert Failed!" );
+        return false;
+      }
+
+      // first delete
+      rows = ExecuteNonQuery($"DELETE FROM member WHERE `id`={id1}");
+      if( rows != 1 )
+      {
+        Console.Write( "delete Failed!" );
+        return false;
+      }
+
+      // second insert
+      var id2 = (int)( System.DateTime.Now.Ticks & 0x00FFFFFF );
+      var date2 = System.DateTime.Now.ToString( "yyyy/MM/dd hh:mm:ss" );
+      rows = ExecuteNonQuery($"INSERT INTO member(`id`, `name`, `dob`) VALUES({id2}, {id2}, \"{date2}\")");
+      if( rows != 1 )
+      {
+        Console.Write( "Insert Failed!" );
+        return false;
+      }
+
+      // first update
+      var id3 = (int)( System.DateTime.Now.Ticks & 0x00FFFFFF );
+      var date3 = System.DateTime.Now.ToString( "yyyy/MM/dd" );
+      rows = ExecuteNonQuery($"UPDATE member set `name`='{id3}', `dob`='{date3}' WHERE `id`='{id2}'");
+      if( rows != 1 )
+      {
+        Console.Write( "Update Failed!" );
         return false;
       }
 
@@ -116,6 +155,35 @@ namespace DatabaseTest
         Console.WriteLine("# ERR: " + ex.Message);
         Console.WriteLine("# ERR: MySQL error code: " + ex.Number);
         return null;
+      }
+      finally
+      {
+        sqlCommand.Dispose();
+      }
+    }
+
+    private static int ExecuteNonQuery(string command)
+    {
+      var sqlCommand = new MySqlCommand("", m_connection);
+
+      try
+      {
+        sqlCommand.CommandText = command;
+
+        var numRows = sqlCommand.ExecuteNonQuery();
+
+        return numRows;
+      }
+      catch (MySqlException ex)
+      {
+        var currentFile = new System.Diagnostics.StackTrace(true).GetFrame(0).GetFileName();
+        var currentLine = new System.Diagnostics.StackTrace(true).GetFrame(0).GetFileLineNumber();
+
+        Console.WriteLine("# ERR: SQLException in " + currentFile + ":" + currentLine);
+        Console.WriteLine("# ERR: " + ex.Message);
+        Console.WriteLine("# ERR: MySQL error code: " + ex.Number);
+
+        return 0;
       }
       finally
       {
